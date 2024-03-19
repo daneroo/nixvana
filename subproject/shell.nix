a8354a156a2cb35ea09e6665fea50e4b4177a062
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-03-19 15:04:17 -0400

    Add shell.nix file for subproject

---
+++ subproject/shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs; [
+     # hello
+     nodejs_21
+     # fastfetch
+  ];
+  shellHook = ''
+    export NPM_CONFIG_PREFIX=~/.npm-global
+    mkdir -p $NPM_CONFIG_PREFIX
+    export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
+    if [ -z "$(command -v pnpm)" ]; then
+      npm install -g pnpm
+    fi
+  '';
+}
