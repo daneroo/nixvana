ffc2e5127786ff3d815908aed6b6e591abf07d01
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-03-21 17:28:00 -0400

    Update direnv settings in post-create-command.sh

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
-# direnv allow
-direnv allow
+# Configure global direnv settings - to allow all .envrc in our container
+mkdir -p ~/.config/direnv
+cat << EOF > ~/.config/direnv/direnv.toml
+# direnv user config
+
+[global]
+# increase warn timeout to 30 minutes
+warn_timeout = "30m"
+
+bash_path = "/bin/bash"
+
+# whitelist all paths for container environment
+[whitelist]
+prefix = [ "/" ]
+EOF
 
 # add direnv hook to .bashrc
 echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+
 
 echo "Post-create command complete."
