845f3dfbd25972fb863626acc4553753c5955a7d
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-03-21 21:32:51 +0000

    Update subproject description and shell hook

--- subproject/flake.nix
+++ subproject/flake.nix
@@ -1,28 +1,36 @@
 {
-  description = "My subproject";
+  description = "My Subproject";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      nativeBuildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
-        nodejs_21
-        # pnpm: will be a global npm install
-        fastfetch
-      ];
+  outputs = { self, nixpkgs }:
+    let
+      mkShell = system: nixpkgs.legacyPackages.${system}.mkShell {
+        nativeBuildInputs = with nixpkgs.legacyPackages.${system}; [
+          nodejs_21
+          # pnpm: will be a global npm install
+          fastfetch
+        ];
 
-      shellHook = ''
-        export NPM_CONFIG_PREFIX=~/.npm-global
-        mkdir -p $NPM_CONFIG_PREFIX
-        export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
-        if [ -z "$(command -v pnpm)" ]; then
-          npm install -g pnpm
-        fi
-        # fastfetch
-        echo "subproject flake activated"
-      '';
+        shellHook = ''
+          # Configure npm for global installations
+          export NPM_CONFIG_PREFIX=~/.npm-global
+          mkdir -p $NPM_CONFIG_PREFIX
+          export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
+          # Install pnpm globally if not already present
+          if [ -z "$(command -v pnpm)" ]; then
+            npm install -g pnpm
+          fi
+          # Display system information on shell start
+          # fastfetch
+          echo "Subproject flake activated"
+        '';
+      };
+    in
+    {
+      devShells.x86_64-linux.default = mkShell "x86_64-linux";
+      devShells.aarch64-linux.default = mkShell "aarch64-linux";
     };
-  };
 }
