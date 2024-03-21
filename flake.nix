b585f12af4afafaf4df39be3874989640ca08b6e
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-03-21 19:57:24 +0000

    Add nixpkgs-fmt package to devcontainer.json and update flake.nix with new devShells

--- flake.nix
+++ flake.nix
@@ -5,39 +5,41 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }: let
-    mkShell = system: nixpkgs.legacyPackages.${system}.mkShell {
-      nativeBuildInputs = with nixpkgs.legacyPackages.${system}; [
-        # Nix Formatter
-        # nixpkgs-fmt
-        # Node.js LTS version
-        nodejs_20
-        # pnpm: will be a global npm install in shellHook
-        deno
-        bun
-        fastfetch
-        docker
-        curl
-        jq
-        tree
-      ];
+  outputs = { self, nixpkgs }:
+    let
+      mkShell = system: nixpkgs.legacyPackages.${system}.mkShell {
+        nativeBuildInputs = with nixpkgs.legacyPackages.${system}; [
+          # Nix Formatter
+          # nixpkgs-fmt
+          # Node.js LTS version
+          nodejs_20
+          # pnpm: will be a global npm install in shellHook
+          deno
+          bun
+          fastfetch
+          docker
+          curl
+          jq
+          tree
+        ];
 
-      shellHook = ''
-        # Configure npm for global installations
-        export NPM_CONFIG_PREFIX=~/.npm-global
-        mkdir -p $NPM_CONFIG_PREFIX
-        export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
-        # Install pnpm globally if not already present
-        if [ -z "$(command -v pnpm)" ]; then
-          npm install -g pnpm
-        fi
-        # Display system information on shell start
-        # fastfetch
-        echo "Top flake activated"
-      '';
+        shellHook = ''
+          # Configure npm for global installations
+          export NPM_CONFIG_PREFIX=~/.npm-global
+          mkdir -p $NPM_CONFIG_PREFIX
+          export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
+          # Install pnpm globally if not already present
+          if [ -z "$(command -v pnpm)" ]; then
+            npm install -g pnpm
+          fi
+          # Display system information on shell start
+          # fastfetch
+          echo "Top flake activated"
+        '';
+      };
+    in
+    {
+      devShells.x86_64-linux.default = mkShell "x86_64-linux";
+      devShells.aarch64-linux.default = mkShell "aarch64-linux";
     };
-  in {
-    devShells.x86_64-linux.default = mkShell "x86_64-linux";
-    devShells.aarch64-linux.default = mkShell "aarch64-linux";
-  };
 }
