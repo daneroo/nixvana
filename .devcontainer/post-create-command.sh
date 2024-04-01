c6c7c002040fc02622b34fa70cd67ec711e7ec72
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-04-01 22:59:11 +0000

    Update devcontainer post-create command

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,26 +1,50 @@
 #!/usr/bin/env bash
 
-# Configure user direnv settings - to allow all .envrc in our container
-echo "Configuring user direnv settings..."
-mkdir -p ~/.config/direnv
-cat << EOF > ~/.config/direnv/direnv.toml
-# direnv user config
 
-[global]
-# increase warn timeout to 30 minutes
-warn_timeout = "30m"
+# From https://github.com/NixOS/nix/issues/6680#issuecomment-1230902525
+# This fixes "suspicious permissions problems" in my nix derivations, however
+#  getfacl /tmp ; before and after reveals no differences
+echo "Setting up ACLs on /tmp"
+sudo sh -c 'apt update -qq && apt install -y -qq acl && setfacl -k /tmp'
 
-# why is this pointing to the base image's bash?
-bash_path = "/bin/bash"
+# Use Determinate installer to install Nix, if not already installed
+if command -v nix >/dev/null 2>&1; then
+  echo "Nix is already installed, skipping Determinate installation"
+else
+  echo "Nix is not installed, installing Nix with Determiate installer - no init - (as $(whoami))"
 
-# whitelist all paths for container environment
-[whitelist]
-prefix = [ "/" ]
-EOF
+  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --no-confirm --init none
 
-echo "Adding direnv hook to .bashrc..."
-echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+  echo "Source the nix-daemon profile"
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+  echo "start nix-daemon (disown/nuhup)"
+  # Notice the '&' to background the process and 'nohup' to prevent the process from being killed when the shell exits
+  sudo -n sh -c '. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh; nohup /nix/var/nix/profiles/default/bin/nix-daemon > /tmp/nix-daemon.log 2>&1 &'
+
+  # echo "Proving the nix-daemon is running"
+  # pidof nix-daemon >/tmp/nix-daemon-pid.log
+
+  # we need a default channel with Determinate installer
+  echo "add a default channel (as $(whoami))"
+  nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
+  nix-channel --update
+  echo "List channels:"
+  nix-channel --list
+fi
+# End of Determinate installer section
+
+# Install home manager from nixpkgs
+nix-env -iA nixpkgs.home-manager
+
+# copy the home-manager configuration to the user's home directory
+rsync -av --progress ./home-manager/ $HOME/.config/home-manager/
+home-manager switch -b backup
+
+echo "Home Manager version: $(home-manager --version)"
+echo "Home Manager generations:"
+home-manager generations
 
 echo "Nix Store size: $(du -sh /nix/store)"
 
-echo "Post-create command complete in devcontainer"
+echo "Post-create (home-manager) command complete in devcontainer"
